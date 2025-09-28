311da86e30420ae68d516ee8842120464113c8b1
darmiel <71837281+darmiel@users.noreply.github.com>
2025-09-28 14:26:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# veinminer-enchant-paper
+
+Modified version of Veinminer enchantment (https://github.com/MiraculixxT/Veinminer) for PaperMC servers.
+This version has some adapted balancing and better integrates in our deployment process.
---
+++ src/main/java/io/d2a/compat/veinminerEnchantPaper/VeinminerEnchantPaper.java
@@ -0,0 +1,12 @@
+package io.d2a.compat.veinminerEnchantPaper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VeinminerEnchantPaper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("VeinminerEnchant for Paper is enabled!");
+    }
+
+}
